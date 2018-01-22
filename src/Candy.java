00e206b111ceee3f52497595fb5cd29f12c215a8
junai <junai@192.168.2.169>
2018-01-21 20:26:51 -0500

    Done (committed from home)

--- src/Candy.java
+++ src/Candy.java
@@ -8,32 +8,34 @@
  *
  * @author malij6756
  */
-public class Candy extends DessertItem {
-
+public class Candy extends DessertItem{
     // private variables for weight and price per pound
     private double weight;
     private int pricePerLbs;
-
+    
     // constructor for updating name, weight, and price per lbs variables
-    public Candy(String name, double weight, int pricePerLbs) {
+    public Candy(String name, double weight, int pricePerLbs)
+    {
         super(name);
         this.weight = weight;
         this.pricePerLbs = pricePerLbs;
     }
 
-    // Printing out the format of the receipt for the candy
+ // Printing out the format of the receipt for the candy
     @Override
-    public String toString() {
-
-        // First line of the candy part of the reciept
-        // Price per lbs converted to dollar format
-        String printCandy = weight + " lbs. @ $" + DessertShoppe.cents2dollarsAndCents(pricePerLbs) + " /lb.";
-        // new line with name of the candy
-        printCandy += "\n";
-        printCandy += super.getName();
-        // converting total cost of candy from cents to dollars
-        String holder = DessertShoppe.cents2dollarsAndCents(getCost());
-        // printing the spaces between the price and name
+    public String toString()
+    {
+       
+        
+       // First line of the candy part of the reciept
+       // Price per lbs converted to dollar format
+       String printCandy = weight + " lbs. @ $" + DessertShoppe.cents2dollarsAndCents(pricePerLbs) + " /lb.";
+       // new line with name of the candy
+       printCandy += "\n";
+       printCandy += super.getName();
+       // converting total cost of candy from cents to dollars
+       String holder = DessertShoppe.cents2dollarsAndCents(getCost());
+       // printing the spaces between the price and name
         for (int i = 0; i < DessertShoppe.RECEIPT_WIDTH - holder.length() - super.getName().length(); i++) {
             printCandy += " ";
         }
